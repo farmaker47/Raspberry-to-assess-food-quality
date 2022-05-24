3c11145cd36763a5561440967e45e4772644cc58
George Soloupis <farmaker47@gmail.com>
2022-05-24 09:34:48 +0300

    Update mq2.py

--- code/mq2.py
+++ code/mq2.py
@@ -9,7 +9,7 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 class MQ2():
 
     ######################### Hardware Related Macros #########################
-    RL_VALUE                     = 5        # define the load resistance on the board, in kilo ohms
+    RL_VALUE                     = 10       # define the load resistance on the board, in kilo ohms
     RO_CLEAN_AIR_FACTOR          = 9.83     # RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
                                             # which is derived from the chart in datasheet
  
